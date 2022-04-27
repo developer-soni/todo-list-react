3ab5d6093d55ac740b6ba64db09633f2c595c9f9
Dev Soni <dsoni@mail.sfsu/edu>
2022-04-27 00:22:50 -0400

    installed reactstrap to use it for add/ edit task modals

---
+++ src/modals/PopUp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Button, Modal, ModalHeader, ModalBody, ModalFooter} from 'reactstrap';
+
+const PopUp = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PopUp
