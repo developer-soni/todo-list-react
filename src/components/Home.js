b392ffdcd289d0214c585f0c61bec37e9cc59158
Dev Soni <dsoni@mail.sfsu/edu>
2022-04-26 23:56:31 -0400

    creating initial structure and installing bootstrap

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+      
+    <div className="">
+      <h1>Dev.eloper's ToDo List</h1>
+      <button className="btn btn-primary">Add Task</button>
+    </div>
+  )
+}
+
+export default Home
