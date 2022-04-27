c218bae99547cd62af2b62dd72fbd02278cd09df
Dev Soni <dsoni@mail.sfsu/edu>
2022-04-27 00:02:30 -0400

    created bootstrap header and add task button

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-      
-    <div className="">
-      <h1>Dev.eloper's ToDo List</h1>
-      <button className="btn btn-primary">Add Task</button>
-    </div>
-  )
-}
+    <>
+      <div className="header text-center">
+        <h2 className="pt-5">Dev.eloper's ToDo List</h2>
+        <button className="btn btn-primary mt-5">Add Task</button>
+      </div>
+      <div className="task-container"></div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
